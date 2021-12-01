a631d6ab0f2768708a3108f67617cd88ff565344
Xu Zhen <azhen@nehza.com>
2021-12-01 16:08:37 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 徐振 on 2021/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SmurfsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmurfsApp.swift
+//  Shared
+//
+//  Created by 徐振 on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct SmurfsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
